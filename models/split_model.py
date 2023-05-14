239e34c4662f4b7f6fb80ff7eb52b2ecb5367956
sunbing7 <bing.sun.2020@phdcs.smu.edu.sg>
2023-05-14 11:48:47 +0800

    test

--- models/split_model.py
+++ models/split_model.py
@@ -50,8 +50,8 @@ def split_model(ori_model, model_name, split_layer=6):
             module3 = modules[5:6]
             module4 = [modules[6]]
 
-            model_1st = nn.Sequential(*[*module1, Relu(), *module2, *module3, Avgpool2d(), Flatten()])
-            model_2nd = nn.Sequential(*module4)
+            model_1st = nn.Sequential(*[*module1, Relu(), *module2, *module3])
+            model_2nd = nn.Sequential(*[Avgpool2d(), Flatten(), *module4])
 
         elif split_layer == 9:
             modules = list(ori_model.children())

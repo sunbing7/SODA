354a0acd68c15f473a976bc2e5bd7cfa0cf9c2b8
sunbing7 <bing.sun.2020@phdcs.smu.edu.sg>
2023-05-18 10:53:34 +0800

    test caltech

--- models/alexnet_impl.py
+++ models/alexnet_impl.py
@@ -4,10 +4,10 @@ import torch.nn as nn
 
 def alexnet(num_classes=10, pretrained=1):
     net = alexnet_tv(pretrained=pretrained)
+    if pretrained:
+        for param in net.parameters():
+            param.requires_grad = False
+
     net.classifier[6] = nn.Linear(4096, num_classes)    # nn.Linear in pytorch is a fully connected layer
                                                         # The convolutional layer is nn.Conv2d
-    if pretrained:
-        for name, param in net.named_parameters():
-            if not 'classifier' in name:
-                param.requires_grad = False
     return net

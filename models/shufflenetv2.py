ecc7bfa008e12d3f943d1b93bc6714eb6260326b
sunbing7 <bing.sun.2020@phdcs.smu.edu.sg>
2023-05-22 20:25:38 +0800

    test asl

---
+++ models/shufflenetv2.py
@@ -0,0 +1,19 @@
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import models
+
+
+def shufflenetv2(num_classes=10, pretrained=1, **kwargs):
+    net = models.shufflenet_v2(pretrained=pretrained)
+    net.aux_logits = False
+
+    if pretrained:
+        for param in net.parameters():
+            param.requires_grad = False
+
+    net.fc = nn.Sequential(
+        nn.Linear(net.fc.in_features, 4096),
+        nn.Linear(4096, num_classes)
+    )
+
+    return net

b3fbd272da57c34acc45710c1cbf3ac19ab17778
sunbing7 <bing.sun.2020@phdcs.smu.edu.sg>
2023-05-24 21:22:57 +0800

    test asl

--- models/selector.py
+++ models/selector.py
@@ -1,54 +1,6 @@
-#from models.wresnet import *
-#from models.resnet import *
-from models.cnn import *
 import os
 
 from collections import OrderedDict
-
-def select_model(dataset,
-                 model_name,
-                 pretrained=False,
-                 pretrained_models_path=None,
-                 n_classes=10,
-                 bn=None):
-
-    assert model_name in ['WRN-16-1', 'WRN-16-2', 'WRN-40-1', 'WRN-40-2', 'ResNet34', 'WRN-10-2', 'WRN-10-1', 'CNN']
-    '''
-    if model_name=='WRN-16-1':
-        model = WideResNet(depth=16, num_classes=n_classes, widen_factor=1, dropRate=0)
-    elif model_name=='WRN-16-2':
-        model = WideResNet(depth=16, num_classes=n_classes, widen_factor=2, dropRate=0)
-    elif model_name=='WRN-40-1':
-        model = WideResNet(depth=40, num_classes=n_classes, widen_factor=1, dropRate=0)
-    elif model_name=='WRN-40-2':
-        model = WideResNet(depth=40, num_classes=n_classes, widen_factor=2, dropRate=0)
-    elif model_name == 'WRN-10-2':
-        model = WideResNet(depth=10, num_classes=n_classes, widen_factor=2, dropRate=0)
-    elif model_name == 'WRN-10-1':
-        model = WideResNet(depth=10, num_classes=n_classes, widen_factor=1, dropRate=0)
-    elif model_name=='ResNet34':
-        model = resnet(depth=32, num_classes=n_classes)
-    elif model_name=='CNN':
-    '''
-    if model_name=='CNN':
-        model = cnn(num_classes=n_classes, bn=bn)
-    else:
-        raise NotImplementedError
-
-    if pretrained:
-        model_path = os.path.join(pretrained_models_path)
-        print('Loading Model from {}'.format(model_path))
-        checkpoint = torch.load(model_path, map_location='cpu')
-        print(checkpoint.keys())
-        load_state_dict(model, orig_state_dict=checkpoint)
-        #model.load_state_dict(checkpoint['state_dict'])
-
-        #print("=> loaded checkpoint '{}' (epoch {}) (accuracy {})".format(model_path, checkpoint['epoch'], checkpoint['best_prec']))
-        print("=> loaded checkpoint '{}' (epoch {}) ".format(model_path, checkpoint['epoch']))
-
-
-    return model
-
 
 def load_state_dict(net, orig_state_dict):
     if 'state_dict' in orig_state_dict.keys():

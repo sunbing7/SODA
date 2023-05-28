0d9fe0bbe891054bb908130adbbd22ea6e964234
sunbing7 <bing.sun.2020@phdcs.smu.edu.sg>
2023-05-28 16:17:24 +0800

    test asl

--- train_backdoor_sem.py
+++ train_backdoor_sem.py
@@ -340,8 +340,8 @@ def train_tune(model, criterion, optimizer, data_loader, adv_loader):
 
     for i, (images, labels) in enumerate(data_loader):
         images_adv, labels_adv = next(iter(adv_loader))
-        _input = torch.cat((images[:10], images_adv[:54]), 0)
-        _output = torch.cat((labels[:10], labels_adv[:54]), 0)
+        _input = torch.cat((images[:20], images_adv[:44]), 0)
+        _output = torch.cat((labels[:20], labels_adv[:44]), 0)
         images = _input
         labels = _output
 

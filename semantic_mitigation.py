154f15464f2647df1ba8bbd570603735339b4e4a
sunbing7 <bing.sun.2020@phdcs.smu.edu.sg>
2023-06-03 17:40:03 +0800

    test asl

--- semantic_mitigation.py
+++ semantic_mitigation.py
@@ -1109,7 +1109,7 @@ def analyze_source_class4(model, model_name, target_class, potential_target, num
             np.savetxt(args.output_dir + "/outstanding_" + "c" + str(source_class) + "_target_" + str(potential_target) + ".txt",
                        temp[:,0].astype(int), fmt="%s")
             ac_mean = np.mean(hidden_test[:, (source_class + 1)])
-            ac_means = ac_means.append(ac_mean)
+            ac_means.append(ac_mean)
 
     idx = np.argsort(ac_means)
     print('[DEBUG]: ac_means{}'.format(idx))
